98d9621db629b3d376872f3875a0ca46e714fa00
ElmerAFM <elmerafm4@gmail.com>
2025-11-23 21:29:04 -0500

    Typescript with sequelize

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,31 +1,47 @@
-import { Sequelize, DataTypes, Model } from "sequelize";
+import { Sequelize, DataTypes, Model, InferAttributes, InferCreationAttributes } from "sequelize";
 
 const sequelize = new Sequelize(process.env.DATABASE_URL!);
 
-const Product = sequelize.define("Product", {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
+class Product extends Model<InferAttributes<Product>, InferCreationAttributes<Product>> {
+  declare id: number | null;
+  declare name: string;
+  declare description: string;
+  declare price: number;
+  declare category: string;
+  declare image: string;
+}
+
+Product.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+    },
+    category: {
+      type: DataTypes.STRING,
+    },
+    image: {
+      type: DataTypes.STRING,
+    },
   },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  description: {
-    type: DataTypes.TEXT,
-  },
-  price: {
-    type: DataTypes.DECIMAL(10, 2),
-    allowNull: false,
-  },
-  category: {
-    type: DataTypes.STRING,
-  },
-  image: {
-    type: DataTypes.STRING,
-  },
-});
+  {
+    sequelize,
+    modelName: "Product",
+    tableName: "products",
+  }
+);
 
 await Product.sync({ force: true });
 
